90100a05ae2a91cc47b49b455b0f4c17f7803fd8
Ahmad <pyhp2017@gmail.com>
2020-08-17 13:12:24 +0430

    Cookie

---
+++ Video26.php
@@ -0,0 +1,53 @@
+<?php
+
+
+/**
+ * A cookie is often used to identify a user. A cookie is a small file 
+ * that the server embeds on the user's computer. Each time the same computer requests
+ *  a page with a browser, 
+ * it will send the cookie too. With PHP, you can both create and retrieve cookie values.
+ */
+
+?>
+
+
+<?php
+// $cookie_name = "user";
+// $cookie_value = "John Doe";
+// setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+
+
+// Delete a Cookie
+// unset($_COOKIE['user']);
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video 26</title>
+</head>
+<body>
+    
+
+<?php
+// if(!isset($_COOKIE[$cookie_name])) {
+//   echo "Cookie named '" . $cookie_name . "' is not set!";
+// } else {
+//   echo "Cookie '" . $cookie_name . "' is set!<br>";
+//   echo "Value is: " . $_COOKIE[$cookie_name];
+// }
+
+
+//Check if Cookies are Enabled
+// if(count($_COOKIE) > 0) {
+//   echo "Cookies are enabled.";
+// } else {
+//   echo "Cookies are disabled.";
+// }
+
+
+?>
+
+</body>
+</html>
